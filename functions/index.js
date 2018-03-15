6d798cbc40b8db8d0834e091689f1d9139849522
Cassandra Le <lenguyetminh1998@gmail.com>
2018-03-14 23:39:53 -0400

    IN PROGRESS: lesson function

--- functions/index.js
+++ functions/index.js
@@ -10,34 +10,50 @@ admin.initializeApp(functions.config().firebase);
 
 //--------- LIST OF DATABASE REF -------
 const dictionaryRef = admin.database().ref('/dictionary/Spanish');
+const practiceRef = admin.database().ref('/practice/Spanish');
+// const dictionaryRef = admin.database().ref('/dictionary');
+
+
 
 //------ DIALOGFLOW ACTIONS -----
-const TRANSLATE_INTENT = "translating"
+const TRANSLATE_INTENT = "translating";
+const LESSON_INTENT = "lessoning";
 
 //--------- PARAMETERS -----------
 const WORD_PARAMETER = "word";
-const LANGUAGE_PARAMETER = "langauge";
+const LANGUAGE_PARAMETER = "language";
 
 exports.lingo = functions.https.onRequest((request, response) => {
-    console.log('headers: ' + JSON.stringify(request.headers));
-    console.log('body: ' + JSON.stringify(request.body));
 
     const assistant = new Assistant({request: request, response: response});
 
     //------------- TRANSLATING FUNCTION -------------
     function translate_func(assistant){
         let word = assistant.getArgument(WORD_PARAMETER);
+        let language = assistant.getArgument(LANGUAGE_PARAMETER);
 
         dictionaryRef.once("value", snap => {
-
+            // const speech = `Okay,` + word + ` in` + language + `is ${snap.val()[word]}`;
             const speech = `Okay,` + word + ` in Spanish is ${snap.val()[word]}`;
             assistant.ask(speech);
         });
     }
 
+
+    //---------------- ADDING LESSONS --------------
+    function lesson_func(assistant){
+        practiceRef.once("value", snap => {
+            const speech = `Here's a question for you. What is lamp in Spanish?`;
+            assistant.ask(speech);
+        });
+    }
+
+
+
     //-------- HANDLE ACTION MAP --------
     let actionMap = new Map();
     actionMap.set(TRANSLATE_INTENT, translate_func);
+    actionMap.set(LESSON_INTENT, lesson_func);
     assistant.handleRequest(actionMap);
 
 
